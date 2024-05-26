17a9670f44b7b70e5722bd7bfb6297073cba18a7
Himanshu-Khadka <iamhimanshu.me@gmail.com>
2024-05-26 01:01:13 -0600

    Web crawler initial push

---
+++ src/web/crawler/webcrawler.java
@@ -0,0 +1,73 @@
+package web.crawler;
+
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class webcrawler {
+    public static void main(String[] args) {
+        //Scanner object
+        Scanner input = new Scanner(System.in);
+        //Prompting for url
+        System.out.println("Enter the URL : ");
+        String url = input.nextLine();
+        //Calling crawl class
+        crawl(url);
+    }
+
+
+    //Crawl method
+    public static void crawl(String URLString){
+        //Array list to hold URL String
+        ArrayList<String> ListOfPendingURLs = new ArrayList<>();
+        ArrayList<String> ListOfVisitedURLs = new ArrayList<>();
+
+        //Adding Starting URLs
+        ListOfPendingURLs.add(URLString);
+
+
+
+    }
+
+    public static ArrayList<String> getSubString(String URLstring){
+        ArrayList<String> list = new ArrayList<>();
+
+        try{
+            URL url = new URL(URLstring);
+            Scanner reader = new Scanner(url.openStream());
+            int current = 0;
+            while (reader.hasNext()){
+                String line = reader.nextLine();
+                current = line.indexOf("http",current);
+
+                while (current>0){
+                    int endIndex = line.indexOf("\"",current);
+
+                    //Ensures correct URL is found
+                    if (endIndex>0){
+                        //Extract URL
+                        list.add(line.substring(current,endIndex));
+                        //Search for the Next URL from the End index
+                        current = line.indexOf("http",endIndex);
+                    }else{
+                        //Sets current to -1 if URL is not found
+                        current = -1;
+                    }
+                }
+            }
+
+        }catch (MalformedURLException ex){
+            System.out.println("URL Error !!!!!");
+            ex.printStackTrace();
+        }catch (IOException iex){
+            System.out.println("Open Stream Error !!!");
+            iex.printStackTrace();
+        }
+        return list;
+    }
+}
+
+
